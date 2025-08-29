a42b486bf3a452e0bb43b0e869df75c5d717ef52
darklord9301-lab <dark.lord.9301@gmail.com>
2025-08-29 09:36:48 +0000

    Update starfield.js

--- modules/starfield.js
+++ modules/starfield.js
@@ -16,7 +16,7 @@ export function initStarfield(scene, camera) {
         brightStarCount: isMobile ? 600 : 900,
         dustParticleCount: isMobile ? 500 : 1000,
         maxDistance: 5000,
-        centerGlowIntensity: 0.2
+        centerGlowIntensity: -0.5
     };
     
     const spaceObjects = [];
